a47a5966ebe5960e7b140abff209f9891ae48751
bwbowen15 <bwbowen15@gmail.com>
2024-08-10 20:57:03 -0400

    Fixed issue with remove function, added more ideas for things to do!

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+//function to put an idea on the screen
 function idea(){
-    //remove();
+    remove();
     const h1 = document.createElement('h1');
     h1.textContent = ideas[random()];
     const ideaArea = document.querySelector('.ideaArea');
     ideaArea.appendChild(h1);
+    //console.log('add');
 }
 
 //generates a random number based on the length of the list
 function random(){
-    return Math.floor(Math.random() * ideas.length-1);
+    let num = Math.floor(Math.random() * ideas.length);
+    //console.log(num);
+    return num;
 }
 
 //removes last suggestion
@@ -16,7 +20,8 @@ function remove(){
     const ideaArea = document.querySelector('.ideaArea');
     let h1 = ideaArea.querySelector('h1');
     ideaArea.removeChild(h1);
+    //console.log('remove');
 }
 
-
-let ideas = new Array('','Take a walk', 'Make some art', 'Watch a movie', 'Read a book', 'Cook some food', 'play a board game');
+//array of ideas to be randomly chosen from
+let ideas = new Array('Take a walk', 'Make some art', 'Watch a movie', 'Read a book', 'Cook some food', 'play a board game', 'Discover some new music', 'Play video games', 'Take a nap', 'Clean your house', 'Go to the gym');
